0c2273bf6eb855a5cc0346367db24b8e5e622684
Kurt Cruz Garcia <kurtc.garcia@gmail.com>
2022-09-23 11:29:15 -0700

    Cambio de imagen a video y estilo al mismo

--- animaciones.js
+++ animaciones.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Ingeniero", "Diseñador Web", "Editor", "Freelancer"];
+const textArray = ["Ingeniero", "Diseñador Web", "Editor", "Fotografo", "Freelancer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
